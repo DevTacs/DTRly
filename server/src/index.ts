b5c4ac210ad16499131e93caa57d67065075328d
Tacs <act.rtacangjr@gmail.com>
2026-05-16 00:24:12 +0800

    feat: connected to mongo atlas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import express from "express"
 import cors from "cors"
+import connectDB from "./configs/db.js"
 
 const app = express()
-
 app.use(cors())
 
-const port = "3000"
+connectDB()
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
